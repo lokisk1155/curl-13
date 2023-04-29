ad129d688e8849bdc8feac5dad78a572f060670c
lokisk1155 <shawn12345d@gmail.com>
2023-04-29 14:57:48 -0400

    update tag and get tag by id

--- test_tag.sh
+++ test_tag.sh
@@ -5,6 +5,19 @@
 # Set the base URL of your API
 BASE_URL="http://localhost:3001/api/tags"
 
-# Get all tags
-echo -e "\033[1;4mTesting GET all tags:\033[0m"
-curl -s -X GET $BASE_URL | jq
+# Create a new tag
+echo -e "\033[1;4mTesting POST a new tag:\033[0m"
+NEW_TAG=$(curl -s -X POST -H "Content-Type: application/json" -d '{"tag_name": "New Tag"}' $BASE_URL)
+NEW_TAG_ID=$(echo $NEW_TAG | jq '.id')
+echo -e "ID: $NEW_TAG_ID Name: New Tag"
+
+# Update a tag by ID
+echo -e "\033[1;4mTesting PUT (update) a tag by ID:\033[0m"
+TAG_ID=1 # Replace with an existing tag ID
+HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" -X PUT -H "Content-Type: application/json" -d '{"tag_name": "Updated Tag Name"}' $BASE_URL/$TAG_ID)
+echo "HTTP Status: $HTTP_STATUS"
+
+# Get updated tag by ID
+echo -e "\033[1;4mTesting GET the updated tag:\033[0m"
+curl -s -X GET $BASE_URL/$TAG_ID | jq
+
