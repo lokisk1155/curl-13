3c7f4451275689f06169693d798c0c888ae12b2a
lokisk1155 <shawn12345d@gmail.com>
2023-04-29 13:39:11 -0400

    post and byid

--- test_api.sh
+++ test_api.sh
@@ -4,6 +4,17 @@
 BASE_URL="http://localhost:3001/api/categories"
 
 # Test GET all categories
-echo "Testing GET all categories:"
-curl -s -X GET $BASE_URL | jq
+echo -e "\033[1;4mTesting GET all categories:\033[0m"
+curl -s -X GET $BASE_URL | jq '.[] | {id: .id, category_name: .category_name, products: [.products[] | {product_id: .id, product_name: .product_name, price: .price, stock: .stock, category_id: .category_id}]}'
+
+# Create new category
+echo -e "\033[1;4mTesting POST a new category:\033[0m"
+NEW_CATEGORY=$(curl -s -X POST -H "Content-Type: application/json" -d '{"category_name": "Test Category"}' $BASE_URL)
+echo $NEW_CATEGORY | jq
+NEW_CATEGORY_ID=$(echo $NEW_CATEGORY | jq '.id')
+
+# Retreive category by ID
+echo -e "\033[1;4mTesting GET a specific category by ID:\033[0m"
+curl -s -X GET $BASE_URL/$NEW_CATEGORY_ID | jq
+
 
