587cc81c5ec97827ab8a018a17b2abd95bd08c70
lokisk1155 <shawn12345d@gmail.com>
2023-04-29 15:24:23 -0400

    better comments 1 more get by id added

--- test_product.sh
+++ test_product.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
-# Set the base URL of your API
+# base url endpoint for this file
 BASE_URL="http://localhost:3001/api/products"
 
-# Create new product
+# post '/' : Create new product
 echo -e "\033[1;4mTesting POST a new product:\033[0m"
 NEW_PRODUCT=$(curl -s -X POST -H "Content-Type: application/json" -d '{"product_name": "Test Product", "price": 10, "stock": 5, "category_id": 1}' $BASE_URL)
 NEW_PRODUCT_ID=$(echo $NEW_PRODUCT | jq '.id')
 echo -e "ID: $NEW_PRODUCT_ID Name: Test Product"
 
-# Retrieve product by ID
+# get '/:id' : Get product by ID
 echo -e "\033[1;4mTesting GET a specific product by ID:\033[0m"
 curl -s -X GET $BASE_URL/$NEW_PRODUCT_ID | jq
 
-# Update the product information
+# put /:id' : Update the product name to Updated Test Product
 echo -e "\033[1;4mTesting PUT (update) a product by ID:\033[0m"
 HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" -X PUT -H "Content-Type: application/json" -d '{"product_name": "Updated Test Product"}' $BASE_URL/$NEW_PRODUCT_ID)
 echo "HTTP Status: $HTTP_STATUS"
 
-# Get updated product by ID
+# get '/:id' : Get updated product by ID
 echo -e "\033[1;4mTesting GET the updated product:\033[0m"
 curl -s -X GET $BASE_URL/$NEW_PRODUCT_ID | jq
 
-# Test DELETE a product by ID
+# delete '/:id' : Delete product by ID
 echo -e "\033[1;4mTesting DELETE a product by ID:\033[0m"
 DELETED_PRODUCT=$(curl -s -X DELETE $BASE_URL/$NEW_PRODUCT_ID)
 echo $NEW_PRODUCT_ID | jq
 
-# Get deleted product by ID (expect an error)
+# get '/:id' : expect an error : Get deleted product by ID 
 echo -e "\033[1;4mTesting GET the deleted product (expecting an error):\033[0m"
 curl -s -X GET $BASE_URL/$NEW_PRODUCT_ID | jq
 
-# Verify deletion against all products
+# get '/' : GetAll : Verify deletion against all products 
 echo -e "\033[1;4mTesting GET all products:\033[0m"
 curl -s -X GET $BASE_URL | jq
--- test_tag.sh
+++ test_tag.sh
@@ -1,37 +1,34 @@
 #!/bin/bash
 
-#!/bin/bash
-
-# Set the base URL of your API
+# base url endpoint for this file
 BASE_URL="http://localhost:3001/api/tags"
 
-# Create a new tag
+# post '/' : Create new tag
 echo -e "\033[1;4mTesting POST a new tag:\033[0m"
 NEW_TAG=$(curl -s -X POST -H "Content-Type: application/json" -d '{"tag_name": "New Tag"}' $BASE_URL)
 NEW_TAG_ID=$(echo $NEW_TAG | jq '.id')
 echo -e "ID: $NEW_TAG_ID Name: Cool new tag"
 
-# Update a tag by ID
+# put /:id' : Update the tag name to Updated tag name
 echo -e "\033[1;4mTesting PUT (update) a tag by ID:\033[0m"
 HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" -X PUT -H "Content-Type: application/json" -d '{"tag_name": "Updated Tag Name"}' $BASE_URL/$NEW_TAG_ID)
 echo "HTTP Status: $HTTP_STATUS"
 
-# Get updated tag by ID
+# get '/:id' : Get updated tag by ID
 echo -e "\033[1;4mTesting GET the updated tag:\033[0m"
 curl -s -X GET $BASE_URL/$NEW_TAG_ID | jq
 
-
-# Delete a tag by ID
+# delete '/:id' : Delete a tag by ID
 echo -e "\033[1;4mTesting DELETE a tag by ID:\033[0m"
 TAG_ID=1 # Replace with an existing tag ID
 HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" -X DELETE $BASE_URL/$NEW_TAG_ID)
 echo "HTTP Status: $HTTP_STATUS"
 
-# Get deleted tag by ID (expect an error)
+# get '/:id' : expect an error : Get deleted tag by ID 
 echo -e "\033[1;4mTesting GET the deleted tag (expecting an error):\033[0m"
 curl -s -X GET $BASE_URL/$NEW_TAG_ID | jq
 
-# Verify deletion against all tags
+# get '/' : GetAll : Verify deletion against all tags
 echo -e "\033[1;4mTesting GET all tags:\033[0m"
 curl -s -X GET $BASE_URL | jq
 
