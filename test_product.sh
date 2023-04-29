5e6d8db967cc40b81f678e0a503b72801d58d1e1
lokisk1155 <shawn12345d@gmail.com>
2023-04-29 14:24:40 -0400

    i think its working

--- test_product.sh
+++ test_product.sh
@@ -13,5 +13,25 @@ NEW_PRODUCT_ID=$(echo $NEW_PRODUCT | jq '.id')
 echo -e "\033[1;4mTesting GET a specific product by ID:\033[0m"
 curl -s -X GET $BASE_URL/$NEW_PRODUCT_ID | jq
 
+# Update the product information
+echo -e "\033[1;4mTesting PUT (update) a product by ID:\033[0m"
+HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" -X PUT -H "Content-Type: application/json" -d '{"product_name": "Updated Test Product"}' $BASE_URL/$NEW_PRODUCT_ID)
+echo "HTTP Status: $HTTP_STATUS"
 
 
+# Get updated product by ID
+echo -e "\033[1;4mTesting GET the updated product:\033[0m"
+curl -s -X GET $BASE_URL/$NEW_PRODUCT_ID | jq
+
+# Test DELETE a product by ID
+echo -e "\033[1;4mTesting DELETE a product by ID:\033[0m"
+DELETED_PRODUCT=$(curl -s -X DELETE $BASE_URL/$NEW_PRODUCT_ID)
+echo $DELETED_PRODUCT | jq
+
+# Get deleted product by ID (expect an error)
+echo -e "\033[1;4mTesting GET the deleted product (expecting an error):\033[0m"
+curl -s -X GET $BASE_URL/$NEW_PRODUCT_ID | jq
+
+# Verify deletion against all products
+echo -e "\033[1;4mTesting GET all products:\033[0m"
+curl -s -X GET $BASE_URL | jq
