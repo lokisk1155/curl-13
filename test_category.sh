8cb5486a81a154910e13286677b0abf7ab788a77
Shawn Mallon <95663040+lokisk1155@users.noreply.github.com>
2023-04-29 15:24:57 -0400

    ImprovedComments(#4)

--- test_category.sh
+++ test_category.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
-# Set the base URL of your API
+# base url endpoint for this file
 BASE_URL="http://localhost:3001/api/categories"
 
-# Create new category
+# post '/' : Create new category
 echo -e "\033[1;4mTesting POST a new category:\033[0m"
 NEW_CATEGORY=$(curl -s -X POST -H "Content-Type: application/json" -d '{"category_name": "Test Category"}' $BASE_URL)
 NEW_CATEGORY_ID=$(echo $NEW_CATEGORY | jq '.id')
 echo -e "ID: $NEW_CATEGORY_ID Name: Test Category"
 
-# Retreive category by ID
+# get '/:id' : Get category by ID
 echo -e "\033[1;4mTesting GET a specific category by ID:\033[0m"
 curl -s -X GET $BASE_URL/$NEW_CATEGORY_ID | jq
 
-# Update the Categories name to Updated Test
+# put /:id' : Update the categories name to Updated Test
 echo -e "\033[1;4mTesting PUT (update) a category by ID:\033[0m"
 HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" -X PUT -H "Content-Type: application/json" -d '{"category_name": "Updated Category Name"}' $BASE_URL/$NEW_CATEGORY_ID)
 echo "HTTP Status: $HTTP_STATUS"
 
-# Get updated category by ID
+# get '/:id' : Get updated category by ID
 echo -e "\033[1;4mTesting GET the updated category:\033[0m"
 curl -s -X GET $BASE_URL/$NEW_CATEGORY_ID | jq
 
-# Test DELETE a category by ID
+# delete '/:id' : Delete a category by ID
 echo -e "\033[1;4mTesting DELETE a category by ID:\033[0m"
 DELETED_CATEGORY=$(curl -s -X DELETE $BASE_URL/$NEW_CATEGORY_ID)
 echo $NEW_CATEGORY_ID | jq
 
-# Get deleted category by ID (expect falsey)
+# get '/:id' : expect an error : Get deleted category by ID 
 echo -e "\033[1;4mTesting GET the deleted category (expecting an error):\033[0m"
 curl -s -X GET $BASE_URL/$NEW_CATEGORY_ID | jq
 
-# Verify deltion against all categories
+# get '/' : GetAll : Verify deletion against all categories
 echo -e "\033[1;4mTesting GET all categories:\033[0m"
 curl -s -X GET $BASE_URL | jq '.[] | {id: .id, category_name: .category_name, products: [.products[] | {product_id: .id, product_name: .product_name, price: .price, stock: .stock, category_id: .category_id}]}'
 
