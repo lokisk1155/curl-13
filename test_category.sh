31ea1c1e1ef8e48c22d598d774ebdc1797d9b438
lokisk1155 <shawn12345d@gmail.com>
2023-04-29 13:46:07 -0400

    delete testing correctly

--- test_category.sh
+++ test_category.sh
@@ -17,4 +17,19 @@ NEW_CATEGORY_ID=$(echo $NEW_CATEGORY | jq '.id')
 echo -e "\033[1;4mTesting GET a specific category by ID:\033[0m"
 curl -s -X GET $BASE_URL/$NEW_CATEGORY_ID | jq
 
+# Test DELETE a category by ID
+echo -e "\033[1;4mTesting DELETE a category by ID:\033[0m"
+DELETED_CATEGORY=$(curl -s -X DELETE $BASE_URL/$NEW_CATEGORY_ID)
+echo $NEW_CATEGORY_ID | jq
+
+# Test GET the deleted category (expecting an error)
+echo -e "\033[1;4mTesting GET the deleted category (expecting an error):\033[0m"
+curl -s -X GET $BASE_URL/$NEW_CATEGORY_ID | jq
+
+# Verify against all categories
+echo -e "\033[1;4mTesting GET all categories:\033[0m"
+curl -s -X GET $BASE_URL | jq '.[] | {id: .id, category_name: .category_name, products: [.products[] | {product_id: .id, product_name: .product_name, price: .price, stock: .stock, category_id: .category_id}]}'
+
+
+
 
