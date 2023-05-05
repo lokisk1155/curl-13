382c09c9fd2deead69256d631fc1ea45a3451ceb
lokisk1155 <shawn12345d@gmail.com>
2023-05-05 09:47:41 -0400

    get all test for categories was wrong

--- test_categories.sh
+++ test_categories.sh
@@ -33,4 +33,4 @@ curl -s -X GET $BASE_URL/$NEW_CATEGORY_ID | jq
 
 # get '/' : GetAll : Verify deletion against all categories
 echo -e "\033[1;4mTesting GET all categories:\033[0m"
-curl -s -X GET $BASE_URL | jq '.[] | {id: .id, category_name: .category_name, products: [.products[] | {product_id: .id, product_name: .product_name, price: .price, stock: .stock, category_id: .category_id}]}'
+curl -s -X GET $BASE_URL | jq
