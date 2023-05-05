2275c0bdd8ec515ec26ba2c6c371baf1667df744
lokisk1155 <shawn12345d@gmail.com>
2023-05-05 16:06:03 -0400

    test name function added to tags

--- test_tags.sh
+++ test_tags.sh
@@ -3,8 +3,12 @@
 # base url endpoint for this file
 BASE_URL="http://localhost:3001/api/tags"
 
+function test_name() {
+  echo -e "\033[1;4m$1:\033[0m"
+}
+
 # post '/' : Create new tag
-echo -e "\033[1;4mTesting POST a new tag:\033[0m"
+test_name "Testing POST a new tag"
 NEW_TAG=$(curl -s -X POST -H "Content-Type: application/json" -d '{"tag_name": "New Tag"}' $BASE_URL)
 NEW_TAG_ID=$(echo $NEW_TAG | jq '.id')
 echo -e "ID: $NEW_TAG_ID Name: Cool new tag"
@@ -14,25 +18,25 @@ echo -e "\033[1;4mTesting GET the updated tag:\033[0m"
 curl -s -X GET $BASE_URL/$NEW_TAG_ID | jq
 
 # put /:id' : Update the tag name to Updated tag name
-echo -e "\033[1;4mTesting PUT (update) a tag by ID:\033[0m"
+test_name "Testing PUT (update) a tag by ID"
 HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" -X PUT -H "Content-Type: application/json" -d '{"tag_name": "Updated Tag Name"}' $BASE_URL/$NEW_TAG_ID)
 echo "HTTP Status: $HTTP_STATUS"
 
 # get '/:id' : Get updated tag by ID
-echo -e "\033[1;4mTesting GET the updated tag:\033[0m"
+test_name "Testing GET the updated tag"
 curl -s -X GET $BASE_URL/$NEW_TAG_ID | jq
 
 # delete '/:id' : Delete a tag by ID
-echo -e "\033[1;4mTesting DELETE a tag by ID:\033[0m"
+test_name "Testing DELETE a tag by ID"
 HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" -X DELETE $BASE_URL/$NEW_TAG_ID)
 echo "HTTP Status: $HTTP_STATUS"
 
 # get '/:id' : expect an error : Get deleted tag by ID 
-echo -e "\033[1;4mTesting GET the deleted tag (expecting an error):\033[0m"
+test_name Testing "GET the deleted tag (expecting an error)"
 curl -s -X GET $BASE_URL/$NEW_TAG_ID | jq
 
 # get '/' : GetAll : Verify deletion against all tags
-echo -e "\033[1;4mTesting GET all tags:\033[0m"
+test_name "Testing GET all tags"
 curl -s -X GET $BASE_URL | jq
 
 
