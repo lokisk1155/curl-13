5ded5fa242276d9b6d3d5b1a4376ec6e0f85e3fc
lokisk1155 <shawn12345d@gmail.com>
2023-05-05 16:04:11 -0400

    if statement not worth it

--- test_products.sh
+++ test_products.sh
@@ -16,32 +16,28 @@ NEW_PRODUCT=$(curl -s -X POST -H "Content-Type: application/json" \
 NEW_PRODUCT_ID=$(echo $NEW_PRODUCT | jq '.id')
 echo -e "ID: $NEW_PRODUCT_ID Name: Test Product"
 
-if [ -n "$NEW_PRODUCT_ID" ]; then
-    # GET '/:id' : Get newly made product by ID
-    test_name "Testing GET a specific product by ID"
-    curl -s -X GET $BASE_URL/$NEW_PRODUCT_ID | jq
+# GET '/:id' : Get newly made product by ID
+test_name "Testing GET a specific product by ID"
+curl -s -X GET $BASE_URL/$NEW_PRODUCT_ID | jq
 
-    # PUT /:id' : Update the product name to Updated Test Product
-    test_name "Testing PUT (update) a product by ID"
-    HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" -X PUT -H "Content-Type: application/json" \
-    -d '{"product_name": "Updated Test Product"}' $BASE_URL/$NEW_PRODUCT_ID)
-    echo "HTTP Status: $HTTP_STATUS"
+# PUT /:id' : Update the product name to Updated Test Product
+test_name "Testing PUT (update) a product by ID"
+HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" -X PUT -H "Content-Type: application/json" \
+-d '{"product_name": "Updated Test Product"}' $BASE_URL/$NEW_PRODUCT_ID)
+echo "HTTP Status: $HTTP_STATUS"
 
-    # GET '/:id' : Get updated product by ID
-    test_name "Testing GET the updated product"
-    curl -s -X GET $BASE_URL/$NEW_PRODUCT_ID | jq
+# GET '/:id' : Get updated product by ID
+test_name "Testing GET the updated product"
+curl -s -X GET $BASE_URL/$NEW_PRODUCT_ID | jq
 
-    # DELETE '/:id' : Delete product by ID
-    test_name "Testing DELETE a product by ID"
-    HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" -X DELETE $BASE_URL/$NEW_PRODUCT_ID)
-    echo "HTTP Status: $HTTP_STATUS"
+# DELETE '/:id' : Delete product by ID
+test_name "Testing DELETE a product by ID"
+HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" -X DELETE $BASE_URL/$NEW_PRODUCT_ID)
+echo "HTTP Status: $HTTP_STATUS"
 
-    # GET '/:id' : Expect an error : Get deleted product by ID
-    test_name "Testing GET the deleted product (expecting an error)"
-    curl -s -X GET $BASE_URL/$NEW_PRODUCT_ID | jq
-else
-  echo "Post request caused an error"
-fi
+# GET '/:id' : Expect an error : Get deleted product by ID
+test_name "Testing GET the deleted product (expecting an error)"
+curl -s -X GET $BASE_URL/$NEW_PRODUCT_ID | jq
 
 # GET '/' : GetAll : Verify deletion against all products
 test_name "Testing GET all products"
