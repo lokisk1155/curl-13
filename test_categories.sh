1661901b7279e8613b6bc1f1676eb817ec44af06
Shawn Mallon <95663040+lokisk1155@users.noreply.github.com>
2023-05-08 10:45:20 -0400

    BetterOutput (#9)
    * conditionals for products
    
    * helper function for conditional+test_name function
    
    * if student's response contains a key called data, key into that for the data instead
    
    * logic works well + added to tags and categories
    
    * helper functions added to both files
    
    * comments added
    
    * conditional logic tested and implemnted to tags
    
    * revert categories and tags for now
    
    * check point its working
    
    * not being hit
    
    * get all test for categories was wrong
    
    * if statement not worth it
    
    * test name function added to tags
    
    * test name added to categories
    
    * print added

--- test_categories.sh
+++ test_categories.sh
@@ -3,34 +3,42 @@
 # base url endpoint for this file
 BASE_URL="http://localhost:3001/api/categories"
 
+function test_name() {
+  echo -e "\033[1;4m$1:\033[0m"
+}
+
+function print() {
+  echo $1 
+}
+
 # post '/' : Create new category
-echo -e "\033[1;4mTesting POST a new category:\033[0m"
+test_name "Testing POST a new category"
 NEW_CATEGORY=$(curl -s -X POST -H "Content-Type: application/json" -d '{"category_name": "Test Category"}' $BASE_URL)
 NEW_CATEGORY_ID=$(echo $NEW_CATEGORY | jq '.id')
-echo -e "ID: $NEW_CATEGORY_ID Name: Test Category"
+print "ID: $NEW_CATEGORY_ID Name: Test Category"
 
 # get '/:id' : Get newly made category by ID
-echo -e "\033[1;4mTesting GET a specific category by ID:\033[0m"
+test_name "Testing GET a specific category by ID"
 curl -s -X GET $BASE_URL/$NEW_CATEGORY_ID | jq
 
 # put /:id' : Update the categories name to Updated Test
-echo -e "\033[1;4mTesting PUT (update) a category by ID:\033[0m"
+test_name "Testing PUT (update) a category by ID"
 HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" -X PUT -H "Content-Type: application/json" -d '{"category_name": "Updated Category Name"}' $BASE_URL/$NEW_CATEGORY_ID)
-echo "HTTP Status: $HTTP_STATUS"
+print "HTTP Status: $HTTP_STATUS"
 
 # get '/:id' : Get updated category by ID
-echo -e "\033[1;4mTesting GET the updated category:\033[0m"
+test_name "Testing GET the updated category"
 curl -s -X GET $BASE_URL/$NEW_CATEGORY_ID | jq
 
 # delete '/:id' : Delete a category by ID
-echo -e "\033[1;4mTesting DELETE a category by ID:\033[0m"
+test_name "Testing DELETE a category by ID"
 HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" -X DELETE $BASE_URL/$NEW_CATEGORY_ID)
-echo "HTTP Status: $HTTP_STATUS"
+print "HTTP Status: $HTTP_STATUS"
 
 # get '/:id' : expect an error : Get deleted category by ID 
-echo -e "\033[1;4mTesting GET the deleted category (expecting an error):\033[0m"
+test_name "Testing GET the deleted category (expecting an error)"
 curl -s -X GET $BASE_URL/$NEW_CATEGORY_ID | jq
 
 # get '/' : GetAll : Verify deletion against all categories
-echo -e "\033[1;4mTesting GET all categories:\033[0m"
-curl -s -X GET $BASE_URL | jq '.[] | {id: .id, category_name: .category_name, products: [.products[] | {product_id: .id, product_name: .product_name, price: .price, stock: .stock, category_id: .category_id}]}'
+test_name "Testing GET all categories"
+curl -s -X GET $BASE_URL | jq
